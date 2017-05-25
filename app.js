2b308cd01489c6f85ade5eb58314112eb8996073
kkhalifah <karen.khalifah@gmail.com>
2017-05-24 23:04:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//module
+var app = angular.module("toDoModule", []);
+//controller
+
+app.controller ("toDoController", function ($scope){
+
+  $scope.lists = [];
+  $scope.displayChore function(chore, priority){
+    //if priority is btwn 1-5 list in numerical order
+    //else log/scope "You have too many priorities and should narrow them down"
+  }
+
+
+//scope to add to page
+
+//scope to filter priority priority cap at 5
+
+//scope to remove items
+})
